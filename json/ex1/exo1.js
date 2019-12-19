8f7a9da8e07d817a69801f3e3362aa250cf9ecc4
J-Gaby <gabriel.desmaris@gmail.com>
2019-12-19 09:05:22 +0000

    json

---
+++ json/ex1/exo1.js
@@ -0,0 +1,12 @@
+var data = {
+    "debug": "on",
+    "title": "Sample Konfabulator",
+    "name": "main_window",
+    "width": "500",
+    "height": "500",
+    "src": "Images/Sun.png",
+    "hOffset": "250",
+    "vOffset": "250",
+    "alignment": "center"
+}
+
